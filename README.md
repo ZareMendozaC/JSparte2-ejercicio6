7c4d0e83c5b2f31c7076f709bd24079135062cca
ZareMendozaC <zare.mendozac@gmail.com>
2016-12-28 10:57:21 -0500

    finish exercise, adding readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # JSparte2-ejercicio6
+# Ejercicio Nº 6
+# Inicio: Make 10
+## Ingreso de datos
+- numero1
+- numero2
+- resultado
+## Proceso
+- Si alguno de los números ingresados es 10 o si la suma de ambos es igual a 10, entonces
+    - return 1, paso las condiciones
+- Caso contrario
+    - return 0, no paso las condiciones!!
+
+*Fin*
--- main.js
+++ main.js
@@ -4,19 +4,19 @@ function calcular()
     var numero2= parseInt(document.getElementById("numero2").value);
     var respuesta = document.getElementById("respuesta");
     var resultado;
-
+  //si alguno de los numeros es 10 o la suma de ambos es 10
   if(( (numero1==10 && numero2!=10) | (numero1!=10 && numero2==10) ) | (numero1+numero2==10) ){
     //pasa, return 1
     // codigo html para mostrar el resultado
-    respuesta.innerHTML = '<h4>Es menor que 21, entonces: </h4><br>'+'<h4>21-'+numero1+' es igual a:  </h4>'+'<h3 class="animated wobble result">'+resultado+'</h3>';
-  }
-    
+    respuesta.innerHTML = '<h4>Paso las condiciones, entonces: </h4><br><br>'+'<h3 class="animated wobble result">VERDADERO</h3>';
+  }  
     else{
 
         // codigo html para mostrar el resultado
-        respuesta.innerHTML = '<h4>Es mayor que 21, entonces: </h4><br>'+'<h4>2*('+numero1+'-21) es igual a:  </h4>'+'<h3 class="animated wobble result">'+resultado+'</h3>';
+        respuesta.innerHTML = '<h4>No paso las condiciones, entonces: </h4><br><br>'+'<h3 class="animated wobble result">FALSO</h3>';
     }
 }
+
 function limpiar(){// esta funcion limpia el campo del número
      numero1 = document.getElementById("numero1").value="";
      numero2 = document.getElementById("numero2").value="";
